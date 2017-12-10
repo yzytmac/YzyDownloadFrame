69461f6685a793e35a9562923fa432af9dea9edc
yzytmac <yzytmac@163.com>
2017-12-10 22:20:31 +0800

    添加注释

--- yzydownloads/src/main/java/com/example/yzydownloads/DownLoadTask.java
+++ yzydownloads/src/main/java/com/example/yzydownloads/DownLoadTask.java
@@ -6,10 +6,9 @@ import android.os.SystemClock;
  * Created by yzy on 2017/12/10.
  */
 
-public class DownLoadTask implements Runnable{
+public class DownLoadTask implements Runnable {
     private DownLoadEntity mEntity;
-    private boolean isPause;
-    private boolean isCancle;
+    private boolean isPause, isCancle;
 
     public DownLoadTask(DownLoadEntity pEntity) {
         mEntity = pEntity;
@@ -23,20 +22,25 @@ public class DownLoadTask implements Runnable{
         isCancle = true;
     }
 
+    @Override
+    public void run() {
+        start();
+    }
+
     private void start() {
         mEntity.status = DownLoadEntity.DownLoadStatus.downloading;
         DataObservable.getInstance().postStatus(mEntity);
 
         mEntity.totalLength = 1024 * 100;
         for (int i = mEntity.currentLength; i < mEntity.totalLength; i++) {
-            if(isCancle || isPause) {
-                mEntity.status = isPause?DownLoadEntity.DownLoadStatus.pause:DownLoadEntity.DownLoadStatus.cancle;
+            if (isCancle || isPause) {
+                mEntity.status = isPause ? DownLoadEntity.DownLoadStatus.pause : DownLoadEntity.DownLoadStatus.cancle;
                 DataObservable.getInstance().postStatus(mEntity);
 
                 // TODO: 2017/12/10
                 return;
             }
-            i+=1024;
+            i += 1024;
             mEntity.currentLength += 1024;
             DataObservable.getInstance().postStatus(mEntity);
             SystemClock.sleep(1000);
@@ -45,8 +49,5 @@ public class DownLoadTask implements Runnable{
         DataObservable.getInstance().postStatus(mEntity);
     }
 
-    @Override
-    public void run() {
-        start();
-    }
+
 }
